5e0eef5fc568612e8903c56331bccbbe4ab4d5b0
Jessica Tong <jessicatong14@gmail.com>
2026-02-19 23:51:31 +0100

    fixed scene reload camera position

--- Assets/Scripts/CameraMovement.cs
+++ Assets/Scripts/CameraMovement.cs
@@ -4,21 +4,21 @@ using UnityEngine;
 public class CameraMovement : MonoBehaviour
 {
 
-    void Start()
-    {
-        // this.transform.position = new Vector3(this.transform.position.x, this.transform.position.y, 0.0F);
-    }
-
     [SerializeField] public Vector3 rotateFrom = new Vector3(0.0F, 0.0F, -45.0F);
     [SerializeField] public Vector3 rotateTo = new Vector3(0.0F, 0.0F, 45.0F);
     [SerializeField] protected float m_frequency = 0.25F;
 
-    protected virtual void Update()
+    private Vector3 startPosition;
+    private Quaternion startRotation;
+    private float timer;
+
+    void Update()
     {
         Quaternion from = Quaternion.Euler(this.rotateFrom);
         Quaternion to = Quaternion.Euler(this.rotateTo);
+        timer += Time.deltaTime;
 
-        float lerp = 0.5F * (1.0F + Mathf.Sin(Mathf.PI * Time.realtimeSinceStartup * this.m_frequency));
+        float lerp = 0.5F * (1.0F + Mathf.Sin(Mathf.PI * timer * this.m_frequency));
         this.transform.localRotation = Quaternion.Lerp(from, to, lerp);
     }
 
