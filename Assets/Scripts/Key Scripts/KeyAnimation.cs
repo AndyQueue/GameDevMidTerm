b44dad69ae6e437db84800be06ab1119889da187
Sophie Martin <>
2026-02-20 17:18:20 +0100

    added scripts

---
+++ Assets/Scripts/Key Scripts/KeyAnimation.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using UnityEngine;
+
+public class KeyAnimation : MonoBehaviour
+{
+    
+    [Header("Sparkle Animation")]
+    [SerializeField] float animationFPS;
+    [SerializeField] Sprite[] spriteAnimation;
+
+    [Header("Bounce Settings")]
+    [SerializeField] float bounceHeight; 
+    [SerializeField] float bounceSpeed;
+
+    private SpriteRenderer sr;
+    private Vector3 startPos;
+    void Start()
+    {
+        sr = GetComponent<SpriteRenderer>();
+        startPos = transform.position;
+        StartCoroutine(AnimationRoutine());
+    }
+    private IEnumerator AnimationRoutine()
+    {
+        float animationTimer = 1f / animationFPS;
+        int currentFrame = 0;
+        while (true) 
+        {
+            sr.sprite = spriteAnimation[currentFrame];
+            currentFrame = (currentFrame + 1) % spriteAnimation.Length;
+            yield return new WaitForSeconds(animationTimer);
+        }
+    }
+
+    void Update()
+    {
+        //uses sin wave to program a light idle bouncing
+        float newY = startPos.y + Mathf.Sin(Time.time * bounceSpeed) * bounceHeight;
+        transform.position = new Vector3(startPos.x, newY, 0);
+    }
+    
+}
