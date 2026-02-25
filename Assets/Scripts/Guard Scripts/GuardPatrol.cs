da4cb49eabdee95313b9f465d4a7d8dd03778d5c
DESKTOP-BNIOURL\Admin <emily34zou@gmail.com>
2026-02-25 14:21:58 +0100

    Started patrolling logic

--- Assets/Scripts/Guard Scripts/GuardPatrol.cs
+++ Assets/Scripts/Guard Scripts/GuardPatrol.cs
@@ -1,16 +1,23 @@
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class GuardPatrol : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
+    [Header ("Patrol Points")]
+    [SerializeField]private Transform leftEdge;
+    [SerializeField] private Transform rightEdge;
 
-    // Update is called once per frame
-    void Update()
+    [Header ("Guard")]
+    [SerializeField] private Transform guard;
+     [Header ("Movment Parameters")]
+    [SerializeField] private float speed;
+
+    private void Update()
     {
-        
+        MoveInDirection(1);
+    }
+    private void MoveInDirection(int _direction)
+    {
+        guard.position = new Vector3(guard.position.x + Time.deltaTime * _direction * speed, guard.position.y, guard.position.z);
     }
 }
