b92fd3500add8cf35b58e17bc45041134f2f8876
Andy Quach <andy.quach@richmond.edu>
2026-03-07 20:35:21 +0100

    moved player audio source

--- Assets/Scripts/Player Scripts/PlayerJumpEffects.cs
+++ Assets/Scripts/Player Scripts/PlayerJumpEffects.cs
@@ -6,17 +6,11 @@ public class PlayerJumpEffects : MonoBehaviour
     [SerializeField] private ParticleSystem jumpParticles;
     private void Awake()
     {
-        if (jumpParticles == null)
-        {
             jumpParticles = GetComponentInChildren<ParticleSystem>();
-        }
     }
 
     public void PlayJumpEffects()
     {
-        if (jumpParticles != null)
-        {
-            jumpParticles.Play();
-        }
+        jumpParticles.Play();
     }
 }
